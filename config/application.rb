7c6406667c2b9747f6bd82734b12d85e8ddb34fb
Jose <josealfredocontreras92@gmail.com>
2017-05-28 22:03:43 -0500

    adding name to pins

--- config/application.rb
+++ config/application.rb
@@ -2,6 +2,7 @@ require_relative 'boot'
 
 require 'rails/all'
 
+config.serve_statc_assests=true;
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
